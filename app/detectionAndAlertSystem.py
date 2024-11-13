110273c4fb7852bb234e1e02f31db675996236c7
Jairo Neto <jairo.neto.dev@gmail.com>
2024-11-13 19:16:23 -0300

    feat: adiciona monitoramento de alterações nas pastas para atualização automática de arquivos

--- app/detectionAndAlertSystem.py
+++ app/detectionAndAlertSystem.py
@@ -1,51 +1,47 @@
-# Instale a biblioteca YOLO
-# pip install ultralytics
-
 import os
+import time
 import cv2
 import threading
 import requests
 import base64
 import shutil
-import glob
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from ultralytics import YOLO
 
-
-# Definição dos caminhos, com variáveis de ambiente e valores padrão
-BASE_PATH = os.getenv('BASE_PATH', '/app')
+# Configurações de Caminhos
+BASE_PATH = os.path.join('Squad8_Chip_Deteccao_De_Imagens/app')
 VOLUME_FRAME_PATH = os.path.join(BASE_PATH, 'volumeFrame')
 VOLUME_FRAME_TEMP_PATH = os.path.join(VOLUME_FRAME_PATH, 'temp')
 VOLUME_FRAME_TREINAMENTO = os.path.join(BASE_PATH, 'volumeFrameTreinamento')
-VOLUME_YOLO = os.path.join(BASE_PATH, 'volumeYolo/best.pt')
+VOLUME_YOLO = os.path.join(BASE_PATH, 'volumeYolo')
 
-# Valores de precisão, obtidos das variáveis de ambiente, com padrão
+# Configurações de Ambiente
 HIGH_PRECISION = float(os.getenv('HIGH_PRECISION', 0.75))
 LOW_PRECISION = float(os.getenv('LOW_PRECISION', 0.5))
 LARGURA_IMAGEM = float(os.getenv('LARGURA_IMAGEM', 1280))
 ALTURA_IMAGEM = float(os.getenv('ALTURA_IMAGEM', 720))
-SEND_IMAGE_TO_API_URL = os.getenv('SEND_IMAGE_TO_API_URL', 'http://localhost:8080/send/')
+SEND_IMAGE_TO_API_URL = os.getenv('SEND_IMAGE_TO_API_URL', 'http://127.0.0.1:5000/upload')
 ID_CLASS_TO_DETECT = int(os.getenv('ID_CLASS_TO_DETECT', 0))
+TIME_TO_SLEEP = 2
 
-# Função para criar diretórios se não existirem
-def existsDirectory(directory):
+# Configuração do Modelo YOLO
+def get_latest_file():
+    return max([os.path.join(VOLUME_YOLO, f) for f in os.listdir(VOLUME_YOLO)], key=os.path.getctime)
+
+LATEST_VOLUME_YOLO = get_latest_file()
+MODEL = YOLO(LATEST_VOLUME_YOLO)
+
+# Funções Utilitárias
+def exists_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-existsDirectory(VOLUME_FRAME_PATH)
-existsDirectory(VOLUME_FRAME_TEMP_PATH)
-existsDirectory(VOLUME_FRAME_TREINAMENTO)
+exists_directory(VOLUME_FRAME_PATH)
+exists_directory(VOLUME_FRAME_TEMP_PATH)
+exists_directory(VOLUME_FRAME_TREINAMENTO)
 
-# Mova as imagens para o diretório temporário
-VOLUME_FRAME_IMAGES = os.path.join(VOLUME_FRAME_PATH, '*.jpg')
-for image in glob.glob(VOLUME_FRAME_IMAGES):
-    shutil.move(image, VOLUME_FRAME_TEMP_PATH)
-
-# Carrega o modelo YOLO
-MODEL = YOLO(VOLUME_YOLO)
-VOLUME_FRAME_TEMP_IMAGES = os.path.join(VOLUME_FRAME_TEMP_PATH, '*.jpg')
-IMAGES_FROM_MODEL = MODEL(VOLUME_FRAME_TEMP_IMAGES, imgz_size=(LARGURA_IMAGEM, ALTURA_IMAGEM))
-
-# Definição da classe de imagem
+# Classe de Representação de Imagem
 class Image:
     def __init__(self, name, image, precision_type, precision):
         self.name = name
@@ -54,43 +50,47 @@ class Image:
         self.type = precision_type if precision_type in valid_types else 'low'
         self.precision = precision
 
-    def formatImage(self):
-        formatted_image = self.result.plot()
-        return formatted_image
+# Funções de Manipulação de Imagem
+def move_images_from_volume_frame_to_temp(limit=10):
+    count = 0
+    for file_name in os.listdir(VOLUME_FRAME_PATH):
+        if file_name.endswith('.jpg'):
+            src_path = os.path.join(VOLUME_FRAME_PATH, file_name)
+            dest_path = os.path.join(VOLUME_FRAME_TEMP_PATH, file_name)
+            
+            shutil.move(src_path, dest_path)
+            print(f"Imagem movida para temp: {dest_path}")
+            
+            count += 1
+            if count >= limit:
+                break
 
-# Função para enviar imagem ao servidor
-def send_image(image_data, file_name, precision):
+def send_image_to_api(image_data, file_name, precision):
     _, buffer = cv2.imencode('.jpg', image_data)
     image_base64 = base64.b64encode(buffer).decode('utf-8')
-
     payload = {
         "file_name": file_name,
         "image": image_base64,
         "precision": precision,
     }
-
     try:
-        response = requests.post(SEND_IMAGE_TO_API_URL, json=payload)
-        response.raise_for_status()
-        print(f"Imagem enviada com sucesso: {file_name} / Status: {response.status_code}")
-        print("Resposta da API:", response.json())
+        requests.post(SEND_IMAGE_TO_API_URL, json=payload)
     except requests.exceptions.RequestException as e:
         print(f"Erro ao enviar imagem: {file_name} / Erro: {e}")
 
-# Função para salvar imagem original para treinamento
 def save_original_image_for_training(image_data, file_name):
     cv2.imwrite(os.path.join(VOLUME_FRAME_TREINAMENTO, file_name), image_data)
 
-# Função para deletar pasta temporária
-def delete_volume_frame_temp(tempPath):
-    if os.path.exists(tempPath):
-        shutil.rmtree(tempPath)
-        print(f"Pasta removida: {tempPath}")
-    else:
-        print(f"A pasta não existe: {tempPath}")
+def delete_volume_frame_temp(temp_path):
+    if os.path.exists(temp_path):
+        shutil.rmtree(temp_path)
+    exists_directory(temp_path)
 
-# Função principal de processamento de imagens
-def process_images(images):
+# Processamento de Imagens
+def process_images():
+    move_images_from_volume_frame_to_temp()
+    images = MODEL(os.path.join(VOLUME_FRAME_TEMP_PATH, '*.jpg'))
+
     for result in images:
         boxes = result.boxes
         file_path = result.path
@@ -100,19 +100,39 @@ def process_images(images):
         if boxes:
             for box in boxes:
                 class_id = box.cls.item()
-
                 if class_id == ID_CLASS_TO_DETECT:
                     precision = box.conf.item() if box.conf.nelement() > 0 else 0
-
                     if precision >= HIGH_PRECISION:
                         formatted_image = result.plot()
                         image = Image(file_name, formatted_image, 'high', precision)
-                        threading.Thread(target=send_image, args=(image.image, image.name, image.precision)).start()
-
+                        threading.Thread(target=send_image_to_api, args=(image.image, image.name, image.precision)).start()
                     elif LOW_PRECISION <= precision < HIGH_PRECISION:
-                        save_original_image_for_training(original_image, file_name)
+                        threading.Thread(target=save_original_image_for_training, args=(original_image, file_name)).start()
 
     delete_volume_frame_temp(VOLUME_FRAME_TEMP_PATH)
 
-# Executa o processamento de imagens
-process_images(IMAGES_FROM_MODEL)
+# Monitoramento de Alterações na Pasta YOLO
+class VolumeYoloHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        global LATEST_VOLUME_YOLO
+        LATEST_VOLUME_YOLO = get_latest_file()
+
+    def on_created(self, event):
+        global LATEST_VOLUME_YOLO
+        LATEST_VOLUME_YOLO = get_latest_file()
+
+# Configuração do Observador de Arquivos
+volume_yolo_handler = VolumeYoloHandler()
+observer = Observer()
+observer.schedule(volume_yolo_handler, path=VOLUME_YOLO, recursive=False)
+observer.start()
+
+# Execução do Processamento de Imagens em Loop
+try:
+    while True:
+        if any(file.endswith('.jpg') for file in os.listdir(VOLUME_FRAME_PATH)):
+            process_images()
+        time.sleep(TIME_TO_SLEEP)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
